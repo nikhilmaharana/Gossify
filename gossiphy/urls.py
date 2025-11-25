fbac6cb3f300d3c7113a18611a37335801a0df64
Nikhil Maharana <nikhilmaharana72@gmail.com>
2025-11-25 21:50:06 +0530

    Fix: Serve media files in production (DEBUG=False)

--- gossiphy/urls.py
+++ gossiphy/urls.py
@@ -11,5 +11,8 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
 ]
 
+# Serve media files in both development and production
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
